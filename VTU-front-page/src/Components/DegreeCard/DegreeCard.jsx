a12a10a9d6c5299b03158d1837ba63c27588f6ae
Lyubomir Georgiev <lyubomirgeorgiev@192.168.100.2>
2024-06-26 19:56:17 +0300

    Added Degree cards

--- VTU-front-page/src/Components/DegreeCard/DegreeCard.jsx
+++ VTU-front-page/src/Components/DegreeCard/DegreeCard.jsx
@@ -1,11 +1,37 @@
 import styles from "./DegreeCard.module.css";
+import PhDhat from "./../../assets/PhD-hat.png"
+import Laptop from "./../../../public/laptopLogo.webp"
+
 
 function DegreeCard() {
-    return (
-        <div className={styles.card}>
-            <img className={styles.cardImage} src="" alt="profile picture"></img>
-            <h2 className={styles.cardTitle}>Бакалавър</h2>
-            <p className={styles.cardText}>4 години</p>
+    return (<div className={styles.cardContainer}>
+            <div className={styles.card}>
+                <img className={styles.cardImage} src={PhDhat} alt="degree picture"></img>
+                <hr/>
+                <h2 className={styles.cardTitle}>Бакалавърски програми</h2>
+                <p className={styles.cardText}>4 години</p>
+            </div>
+
+            <div className={styles.card}>
+                <img className={styles.cardImage} src={PhDhat} alt="degree picture"></img>
+                <hr/>
+                <h2 className={styles.cardTitle}>Магистърски програми</h2>
+                <p className={styles.cardText}>1.5 години</p>
+            </div>
+
+            <div className={styles.card}>
+                <img className={styles.cardImage} src={PhDhat} alt="degree picture"></img>
+                <hr/>
+                <h2 className={styles.cardTitle}>Докторски програми</h2>
+                <p className={styles.cardText}>3 години</p>
+            </div>
+
+            <div className={styles.card}>
+                <img className={styles.cardImage} src={Laptop} alt="degree picture"></img>
+                <hr/>
+                <h2 className={styles.cardTitle}>Дистанционно обучение</h2>
+                <p className={styles.cardText}>4 години</p>
+            </div>
         </div>
     )
 }
