9a64466f6c1fa34b983c5701573cb7db40d0e865
Lyubomir Georgiev <lyubomirgeorgiev@192.168.100.2>
2024-06-27 23:25:43 +0300

    Added more icons for footer

--- VTU-front-page/src/Components/Footer/Footer.jsx
+++ VTU-front-page/src/Components/Footer/Footer.jsx
@@ -1,4 +1,16 @@
 import styles from './Footer.module.css'
+import FacebookLogo from './../../../public/FacebookLogo.svg'
+import InstagramLogo from './../../../public/InstagramLogo.svg'
+import XLogo from './../../../public/XLogo.svg'
+import YoutubeLogo from './../../../public/YoutubeLogo.svg'
+import LinkedIn from './../../../public/LinkedInLogo.svg'
+
+
+
+import BankLogo from './../../../public/BankLogo.svg'
+import TelephoneLogo from './../../../public/TelephoneLogo.svg'
+import EmailLogo from './../../../public/EmailLogo.svg'
+import MapLogo from './../../../public/MapLogo.svg'
 
 
 function Footer() {
@@ -6,15 +18,21 @@ function Footer() {
         <div className={styles.footer}>
             <div className={styles.upperFooter}>
                 <ul className={styles.upperFooterList}>
-                    <li>Facebook</li>
-                    <li>X</li>
-                    <li>Instagram</li>
-                    <li>LinkedIn</li>
-                    <li>Youtube</li>
+                    <li><img src={FacebookLogo} alt={"Facebook logo"}></img> Facebook</li>
+                    <li><img src={XLogo} alt={"X logo"}></img> X</li>
+                    <li><img src={InstagramLogo} alt={"Facebook logo"}></img> Instagram</li>
+                    <li><img src={LinkedIn} alt={"Facebook logo"}></img> LinkedIn</li>
+                    <li><img src={YoutubeLogo} alt={"Facebook logo"}></img> Youtube</li>
                 </ul>
             </div>
             <div className={styles.lowerFooter}>
 
+                <ul className={styles.Contacts}>
+                    <li><img src={BankLogo} alt={"Bank logo"}/></li>
+                    <li><img src={TelephoneLogo} alt={"Telephone logo"}/></li>
+                    <li><img src={EmailLogo} alt={"Email logo"}/></li>
+                    <li><img src={MapLogo} alt={"Map logo"}/></li>
+                </ul>
             </div>
         </div>
     )
