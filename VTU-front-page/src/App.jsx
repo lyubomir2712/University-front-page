72ca4bae0840409c7be5538d715ead3d09b9d48f
Lyubomir Georgiev <lyubomirgeorgiev@192.168.100.2>
2024-06-27 22:41:29 +0300

    Added new logo's for footer also updated the footer component

--- VTU-front-page/src/App.jsx
+++ VTU-front-page/src/App.jsx
@@ -3,6 +3,8 @@ import DegreeCard from './Components/DegreeCard/DegreeCard.jsx'
 import UniversityAdditionalInfo from "./Components/UniversityAdditionalInfo/UniversityAdditionalInfo.jsx";
 import News from "./Components/News/News.jsx"
 import Carousel from "./Components/Carousel/Carousel.jsx";
+import Footer from "./Components/Footer/Footer.jsx"
+
 function App() {
 
 
@@ -16,6 +18,7 @@ function App() {
           <UniversityAdditionalInfo/>
 
           <News/>
+          <Footer/>
       </>
   )
 }
--- VTU-front-page/src/Components/Footer/Footer.jsx
+++ VTU-front-page/src/Components/Footer/Footer.jsx
@@ -1,5 +1,23 @@
-export function Footer() {
+import styles from './Footer.module.css'
+
+
+function Footer() {
     return (
-        <></>
+        <div className={styles.footer}>
+            <div className={styles.upperFooter}>
+                <ul className={styles.upperFooterList}>
+                    <li>Facebook</li>
+                    <li>X</li>
+                    <li>Instagram</li>
+                    <li>LinkedIn</li>
+                    <li>Youtube</li>
+                </ul>
+            </div>
+            <div className={styles.lowerFooter}>
+
+            </div>
+        </div>
     )
 }
+
+export default Footer
