15cd5d5323fe8c6d8355a7c4f58abe2339c1c6ee
Lyubomir Georgiev <lyubomirgeorgiev@Lyubomirs-MacBook-Pro.local>
2024-06-29 00:29:15 +0300

    updated the cards. I have to update the Info section, also to add news section and fix the language change bug. If i have enough time will do some JS animations on the page

--- VTU-front-page/src/Components/DegreeCard/DegreeCard.jsx
+++ VTU-front-page/src/Components/DegreeCard/DegreeCard.jsx
@@ -8,32 +8,36 @@ import Laptop from "../../assets/Laptop-logo.png"
 function DegreeCard() {
     return (<div className={styles.cardContainer}>
             <div className={styles.card}>
-                <img className={styles.cardImage} src={BachelorHat} alt="degree picture"></img>
+                <a href={"#"}><img className={styles.cardImage} src={BachelorHat} alt="degree picture"></img></a>
                 <hr/>
-                <h2 className={styles.cardTitle}>Бакалавърски програми</h2>
-                <p className={styles.cardText}>4 години</p>
+                <br/>
+                <a href={"#"}><h2 className={styles.cardTitle}>Бакалавърски програми</h2></a>
+                {/*<p className={styles.cardText}>4 години</p>*/}
             </div>
 
             <div className={styles.card}>
-                <img className={styles.cardImage} src={MastersHat} alt="degree picture"></img>
+                <a href={"#"}><img className={styles.cardImage} src={MastersHat} alt="degree picture"></img></a>
                 <hr/>
-                <h2 className={styles.cardTitle}>Магистърски програми</h2>
-                <p className={styles.cardText}>1.5 години</p>
+                <br/>
+                <a href={"#"}><h2 className={styles.cardTitle}>Магистърски програми</h2></a>
+                {/*<p className={styles.cardText}>1.5 години</p>*/}
             </div>
 
             <div className={styles.card}>
-                <img className={styles.cardImage} src={PhDhat} alt="degree picture"></img>
+                <a href={"#"}><img className={styles.cardImage} src={PhDhat} alt="degree picture"></img></a>
                 <hr/>
-                <h2 className={styles.cardTitle}>Докторски програми</h2>
+                <br/>
+                <a href={"#"}><h2 className={styles.cardTitle}>Докторски програми</h2></a>
 
-                <p className={styles.cardText}>3 години</p>
+                {/*<p className={styles.cardText}>3 години</p>*/}
             </div>
 
             <div className={styles.card}>
-                <img className={styles.cardImage} src={Laptop} alt="degree picture"></img>
+                <a href={"#"}><img className={styles.cardImage} src={Laptop} alt="degree picture"></img></a>
                 <hr/>
-                <h2 className={styles.cardTitle}>Дистанционно обучение</h2>
-                <p className={styles.cardText}>4 години</p>
+                <br/>
+                <a href={"#"}><h2 className={styles.cardTitle}>Дистанционно обучение</h2></a>
+                {/*<p className={styles.cardText}>4 години</p>*/}
             </div>
         </div>
     )
