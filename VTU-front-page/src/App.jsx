d980df412b80ce8c945fcc6d99bdd7c062a109b7
Lyubomir Georgiev <lyubomirgeorgiev@192.168.100.2>
2024-06-28 12:36:30 +0300

    Renamed the news component to event section and added content inside it

--- VTU-front-page/src/App.jsx
+++ VTU-front-page/src/App.jsx
@@ -1,7 +1,7 @@
 import NavBar from './Components/NavBar/NavBar.jsx'
 import DegreeCard from './Components/DegreeCard/DegreeCard.jsx'
 import UniversityAdditionalInfo from "./Components/UniversityAdditionalInfo/UniversityAdditionalInfo.jsx";
-import News from "./Components/News/News.jsx"
+import EventsSection from "./Components/EventsSection/EventsSection.jsx"
 import Carousel from "./Components/Carousel/Carousel.jsx";
 import Footer from "./Components/Footer/Footer.jsx"
 
@@ -17,7 +17,7 @@ function App() {
 
           <UniversityAdditionalInfo/>
 
-          <News/>
+          <EventsSection/>
           <Footer/>
       </>
   )
---
+++ VTU-front-page/src/Components/EventsSection/EventsSection.jsx
@@ -0,0 +1,141 @@
+import React from 'react';
+import { styled } from '@mui/system';
+
+const EventsSectionContainer = styled('div')({
+    backgroundColor: 'white',
+    padding: '20px',
+    margin: '20px',
+    borderRadius: '8px',
+    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+});
+
+const Heading = styled('h2')({
+    fontSize: '24px',
+    marginBottom: '20px',
+});
+
+const Event = styled('div')({
+    display: 'flex',
+    marginBottom: '20px',
+    padding: '20px',
+    borderBottom: '1px solid #ddd',
+    '&:last-child': {
+        borderBottom: 'none',
+    },
+});
+
+const EventDate = styled('div')({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'purple',
+    color: 'white',
+    padding: '10px',
+    marginRight: '20px',
+    borderRadius: '4px',
+});
+
+const EventDateSpan = styled('span')({
+    display: 'block',
+    fontSize: '16px',
+    textAlign: 'center',
+});
+
+const EventDateSpanBold = styled(EventDateSpan)({
+    fontSize: '24px',
+    fontWeight: 'bold',
+});
+
+const EventDetails = styled('div')({
+    h3: {
+        margin: '0',
+        fontSize: '20px',
+        color: '#333',
+    },
+    p: {
+        margin: '5px 0',
+        color: '#666',
+    },
+});
+
+const ViewAllBtn = styled('button')({
+    display: 'block',
+    margin: '0 auto',
+    padding: '10px 20px',
+    backgroundColor: 'purple',
+    color: 'white',
+    border: 'none',
+    borderRadius: '4px',
+    cursor: 'pointer',
+    fontSize: '16px',
+    '&:hover': {
+        backgroundColor: '#a282d5',
+    },
+});
+
+function EventsSection() {
+    return (
+        <EventsSectionContainer>
+            <Heading>Събития</Heading>
+            <Event>
+                <EventDate>
+                    <div>
+                        <EventDateSpan>НАЧАЛО</EventDateSpan>
+                        <EventDateSpanBold>25</EventDateSpanBold>
+                        <EventDateSpan>ЮНИ <hr/></EventDateSpan>
+                    </div>
+                    <div>
+                        <EventDateSpan>КРАЙ</EventDateSpan>
+                        <EventDateSpanBold>28</EventDateSpanBold>
+                        <EventDateSpan>ЮНИ</EventDateSpan>
+                    </div>
+                </EventDate>
+                <EventDetails>
+                    <h3>Академия по предприемачество в земеделието</h3>
+                    <p>25 ЮНИ 2024 - 28 ЮНИ 2024 | ВЕЛИКОТЪРНОВСКИ УНИВЕРСИТЕТ - &quot; СВ. СВ. КИРИЛ И МЕТОДИЙ &quot;</p>
+                    <p>Първото издание ще се проведе от 25 до 28 юни 2024 година - град Велико Търново във Великотърноски университет - &quot; Св. св. Кирил и Методий &quot;</p>
+                </EventDetails>
+            </Event>
+            <Event>
+                <EventDate>
+                    <div>
+                        <EventDateSpanBold>02</EventDateSpanBold>
+                        <EventDateSpan>ЮЛИ</EventDateSpan>
+                    </div>
+                </EventDate>
+                <EventDetails>
+                    <h3>Публична защита на дисертационен труд</h3>
+                    <p>02 ЮЛИ 2024 | 13:00 - 15:00 | ЗАЛА 320</p>
+                </EventDetails>
+            </Event>
+            <Event>
+                <EventDate>
+                    <div>
+                        <EventDateSpanBold>02</EventDateSpanBold>
+                        <EventDateSpan>ЮЛИ</EventDateSpan>
+                    </div>
+                </EventDate>
+                <EventDetails>
+                    <h3>Публична защита на дисертационен труд</h3>
+                    <p>02 ЮЛИ 2024 | 14:00 - 16:00 | ЗАЛА 205</p>
+                </EventDetails>
+            </Event>
+            <Event>
+                <EventDate>
+                    <div>
+                        <EventDateSpanBold>18</EventDateSpanBold>
+                        <EventDateSpan>ОКТ</EventDateSpan>
+                    </div>
+                </EventDate>
+                <EventDetails>
+                    <h3>Конференция на тема Правото и бизнесът в съвременното общество</h3>
+                    <p>18 ОКТ 2024 | 09:30 - 13:00 | ЗАЛА 1</p>
+                </EventDetails>
+            </Event>
+            <ViewAllBtn>Виж всички</ViewAllBtn>
+        </EventsSectionContainer>
+    );
+}
+
+export default EventsSection;
