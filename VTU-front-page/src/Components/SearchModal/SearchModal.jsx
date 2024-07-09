bbe92e0ff34724d6ad959a18588a59eb8eadb113
Lyubomir Georgiev <lyubomirgeorgiev@192.168.100.2>
2024-07-09 18:21:19 +0300

    made new search modal

--- VTU-front-page/src/Components/SearchModal/SearchModal.jsx
+++ VTU-front-page/src/Components/SearchModal/SearchModal.jsx
@@ -5,11 +5,12 @@ import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import SearchIcon from "../../../public/SearchIcon.svg";
 
-import SearchIconIcon from "../../../public/SearchIcon.svg";
+import SearchIcon2 from "../../../public/SearchIcon2.svg";
 import styles from './SearchModal.module.css'
 import {useTranslation} from "react-i18next";
 import {useState} from "react";
 import i18n from '../LanguageChange/LanguageChange.js';
+
 
 
 const style = {
@@ -18,7 +19,7 @@ const style = {
     left: '50%',
     transform: 'translate(-50%, -50%)',
     width: 400,
-    bgcolor: 'background.paper',
+    bgcolor: 'transparent',
     border: '2px solid #000',
     borderRadius: '0.5rem',
     boxShadow: 24,
@@ -29,7 +30,13 @@ const modal = {
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
+
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
 }
+
 
 
 export default function SearchModal() {
@@ -52,29 +59,38 @@ export default function SearchModal() {
 
     return (
         <div>
-            <img src={SearchIcon} alt={"Search icon"} onClick={handleOpen} />
-            {/*<Button onClick={handleOpen}>Open modal</Button>*/}
+            <img src={SearchIcon2} alt={"Search icon"} onClick={handleOpen} />
             <Modal
                 open={open}
                 onClose={handleClose}
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
+                BackdropProps={{
+                    style: { backgroundColor: 'transparent' }
+                }}
+
+                disableEnforceFocus
+                disableScrollLock
             >
-                <Box sx={style}>
-                    <Typography id="modal-modal-title" variant="h6" component="h2">
-                        {t("Търсене")}
-                    </Typography>
+                {/*<Box sx={style}>*/}
+                    {/*<Typography id="modal-modal-title" variant="h6" component="h2">*/}
+                    {/*    {t("Търсене")}*/}
+                    {/*</Typography>*/}
+
                     <Typography style={modal} id="modal-modal-description" sx={{mt: 2}}>
-                        <input className={styles.modalInput} type={"text"}/>
-                        <select className={styles.modalDropdown}>
-                            <option>{t("Всички")}</option>
-                            <option>{t("Новини и събития")} </option>
-                            <option>{t("Преподаватели и служители")}</option>
-                            <option>{t("Страници")}</option>
-                        </select>
+                        <input className={styles.modalInput} type={"text"} placeholder={"Търсене"}/>
+
+
+
+                        {/*<select className={styles.modalDropdown}>*/}
+                        {/*    <option>{t("Всички")}</option>*/}
+                        {/*    <option>{t("Новини и събития")} </option>*/}
+                        {/*    <option>{t("Преподаватели и служители")}</option>*/}
+                        {/*    <option>{t("Страници")}</option>*/}
+                        {/*</select>*/}
                         <button className={styles.modalButton}><img src={SearchIcon} alt={"search icon"}/></button>
                     </Typography>
-                </Box>
+                {/*</Box>*/}
             </Modal>
         </div>
     );
